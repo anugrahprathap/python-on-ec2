49cd75c63fb5bd671923cc184150819afb03ce0b
Anugrah P <84971294+anugrahprathap@users.noreply.github.com>
2023-12-03 23:33:52 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # This file is scripts/install_dependencies.sh
-
+chmod +x scripts/start_app.sh
+chmod +x scripts/stop_app.sh
+chmod +x scripts/install_dependencies.sh
 pip install -r requirements.txt
