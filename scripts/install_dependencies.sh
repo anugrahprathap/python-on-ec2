9d3ebd62f3bd6717ad439e981eb5e7035bbd9db2
Anugrah P <84971294+anugrahprathap@users.noreply.github.com>
2023-12-04 10:31:34 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # This file is scripts/install_dependencies.sh
-chmod 777 scripts/start_app.sh
-chmod 777 scripts/stop_app.sh
-chmod 777 scripts/install_dependencies.sh
-pip install -r requirements.txt
+
+pip install -r /home/ubuntu/requirements.txt
