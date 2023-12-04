b825a466ce967eadc99715634b92471ea3bfd4f2
Anugrah P <84971294+anugrahprathap@users.noreply.github.com>
2023-12-04 10:20:07 +0530

    Delete scripts directory

--- scripts/install_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This file is scripts/install_dependencies.sh
-chmod 777 scripts/start_app.sh
-chmod 777 scripts/stop_app.sh
-chmod 777 scripts/install_dependencies.sh
-pip install -r requirements.txt
--- scripts/start_app.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-set -e
-cd  /home/ec2-user/
-gunicorn -w 4 run:app -b 0.0.0.0:8000
