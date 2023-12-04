f1c5072c6bc824fd79fd23a2404c8e9f26aec5ab
anugrahprathap <anugrahprathap@gmial.com>
2023-12-04 13:25:58 +0530

    add html page

--- dockerfiles/app.py
+++ dockerfiles/app.py
@@ -1,12 +1,12 @@
 # app.py
 
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is my Flask web server.'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
