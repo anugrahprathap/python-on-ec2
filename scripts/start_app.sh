a774fb245348f335051d7ee4c371d8e7aeb0ec0d
Anugrah P <84971294+anugrahprathap@users.noreply.github.com>
2023-12-04 11:03:02 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 pip install gunicorn
 cd  /home/ubuntu/
-/home/ubuntu/.local/bin/gunicorn -w 4 run:app -b 0.0.0.0:8000
+/home/ubuntu/.local/bin/gunicorn -w 4 run:app -b 0.0.0.0:8000 &
 
